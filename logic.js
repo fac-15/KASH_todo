a7c070956e77b437c8377f67581ba04c92175c25
Hannah <hannahstewart23@yahoo.co.uk>
2018-11-07 16:10:53 +0000

    making the test work

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 // Part 1. Fill in any missing parts of the todoFunction object!
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
-
+var todoFunctions= require('./logic.js');
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
@@ -14,15 +14,15 @@ var todoFunctions = {
 
     return incrementCounter;
   })(),
-  
-  //cloneArrayOfObjects will create a copy of the todos array 
+
+  //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
     return todos.map(function(todo){
       return JSON.parse(JSON.stringify(todo));
     });
   },
-  
+
   addTodo: function(todos, newTodo) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
@@ -51,7 +51,7 @@ var todoFunctions = {
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details: 
+// See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
   module.exports = todoFunctions;
