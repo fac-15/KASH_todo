7a91e46374a63c617d25ae4aee6825446c2b07a8
aterna01 <ssaniok90@mail.ru>
2018-11-08 11:31:09 +0000

    add addto func

--- logic.js
+++ logic.js
@@ -1,7 +1,6 @@
 // Part 1. Fill in any missing parts of the todoFunction object!
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
-var todoFunctions= require('./logic.js');
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
@@ -18,18 +17,28 @@ var todoFunctions = {
   //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
-    return todos.map(function(todo){
+    return todos.map(function(todo) {
       return JSON.parse(JSON.stringify(todo));
     });
   },
-
   addTodo: function(todos, newTodo) {
+    let arrayCopy = todoFunctions.cloneArrayOfObjects(todos);
+    let id = todoFunctions.generateId(newTodo);
+    return arrayCopy.concat({ description: newTodo, id: id });
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
   },
   deleteTodo: function(todos, idToDelete) {
+    let arrayCopy = todoFunctions.cloneArrayOfObjects(todos);
+    let result = array.filter(filterByID);
+
+    function filterByID(item) {
+      if (item.id === idToDelete) {
+        delete array.item;
+      }
+    }
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
@@ -45,14 +54,13 @@ var todoFunctions = {
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
     // sortFunction will have same signature as the sort function in array.sort
     // hint: array.slice, array.sort
-  },
+  }
 };
-
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
 // See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = todoFunctions;
 }
--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-const todoFunctions = require('./logic.js');
+const todoFunctions = require("./logic.js");
 
-test('test if generateId works', () => {
+test("test if generateId works", () => {
   expect(todoFunctions.generateId()).toBe(1);
+});
+
+// test("test if it is an array", () => {
+//   expect(Array.isArray(todoFunctions.addTodo())).toBe(true);
+// });
+
+test("test if updated array is returned", () => {
+  expect(todoFunctions.addTodo([1, 2, 3, 4])).toEqual([1, 2, 3, { id: 2 }]);
 });
 
 // test('test if cloneArrayOfObjects works', () => {
